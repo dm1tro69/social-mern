c101166e8b55eb0658efc22848f9b8daef9eadb0
Dmytro Voronov <dimolg22@gmail.com>
2022-12-26 12:50:16 +0200

    add redux-persist

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import {Route, Routes} from "react-router-dom";
+import LoginPage from "./scenes/loginPage";
+import HomePage from "./scenes/homePage";
+import ProfilePage from "./scenes/profilePage";
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+import authReducer from './state/index'
+
 
 function App() {
   return (
     <div className="app">
-
+      <Routes>
+        <Route path={'/'} element={<LoginPage/>}/>
+        <Route path={'/home'} element={<HomePage/>}/>
+        <Route path={'/profile/:userId'} element={<ProfilePage/>}/>
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {persistReducer, persistStore, FLUSH, PERSIST, PAUSE, PURGE, REHYDRATE} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+import {PersistGate} from "redux-persist/integration/react";
+import {configureStore} from "@reduxjs/toolkit";
+import authReducer from "./state";
+import {Provider} from "react-redux";
+
+
+const persistConfig = {key: 'root', storage, version: 1}
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [FLUSH, PERSIST, PAUSE, PURGE, REHYDRATE]
+            }
+        })
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
   <BrowserRouter>
+      <PersistGate loading={null} persistor={persistStore(store)}>
     <App />
+      </PersistGate>
   </BrowserRouter>
+    </Provider>
 );
 
